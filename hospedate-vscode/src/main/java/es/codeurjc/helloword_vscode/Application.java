7cd49322b84eed9a56056d8e64ca935bbf4aef07
feerr5 <fernandomonteromolina6@gmail.com>
2026-02-17 09:34:43 +0100

    Resolved sintax errors

--- hospedate-vscode/src/main/java/es/codeurjc/helloword_vscode/Application.java
+++ hospedate-vscode/src/main/java/es/codeurjc/helloword_vscode/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HellowordVscodeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellowordVscodeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
