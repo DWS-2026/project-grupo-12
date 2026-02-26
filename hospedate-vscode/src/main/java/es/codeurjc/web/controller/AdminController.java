d9c2e192c19097038eeee5c37cccee72be64849d
feerr5 <fernandomonteromolina6@gmail.com>
2026-02-26 12:25:43 +0100

    Add PostMapping import to AdminController

--- hospedate-vscode/src/main/java/es/codeurjc/web/controller/AdminController.java
+++ hospedate-vscode/src/main/java/es/codeurjc/web/controller/AdminController.java
@@ -1,6 +1,7 @@
 package es.codeurjc.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
