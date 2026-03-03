d16ff2a0becf99109d80090d550fc025181f2fd9
0L1V3R5 <o.infante.2024@alumnos.urjc.es>
2026-03-03 10:38:32 +0100

    Add attributes to reserve model, add Id to models, and add dependecies to pom.xml

--- hospedate-vscode/src/main/java/es/codeurjc/web/model/Hotel.java
+++ hospedate-vscode/src/main/java/es/codeurjc/web/model/Hotel.java
@@ -1,7 +1,15 @@
 package es.codeurjc.web.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Hotel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
     private String city;
     private double price;
--- hospedate-vscode/src/main/java/es/codeurjc/web/model/Review.java
+++ hospedate-vscode/src/main/java/es/codeurjc/web/model/Review.java
@@ -1,5 +1,15 @@
 package es.codeurjc.web.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Review {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
 }
